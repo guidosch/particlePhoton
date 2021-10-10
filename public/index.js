2d3d4e6b46cb378543f9bf1064c340113f10658e
Guido Schnider <guido.schnider@gmail.com>
2021-10-10 23:39:16 +0200

    Update room name right after selection.

--- public/index.js
+++ public/index.js
@@ -12,9 +12,9 @@ var selectedRoom;
 var labelMap = new Map();
 
 var deviceMap = new Map();
-deviceMap.set("B14_2_21","270039001447363336383438");
-deviceMap.set("B14_2_25","3e0038000e47363336383437");
-deviceMap.set("B14_2_26","340038001247363336383437");
+deviceMap.set("B14_2_21", "270039001447363336383438");
+deviceMap.set("B14_2_25", "3e0038000e47363336383437");
+deviceMap.set("B14_2_26", "340038001247363336383437");
 
 /** 
 labelMap.set("title","Roller automation");
@@ -24,11 +24,11 @@ labelMap.set("button-close","down");
 labelMap.set("status-relays","Rollers down?");
 */
 
-labelMap.set("title","Fenster automation");
-labelMap.set("document-title","Fenster open/close");
-labelMap.set("button-open","open");
-labelMap.set("button-close","close");
-labelMap.set("status-relays","Fenster offen?");
+labelMap.set("title", "Fenster automation");
+labelMap.set("document-title", "Fenster open/close");
+labelMap.set("button-open", "open");
+labelMap.set("button-close", "close");
+labelMap.set("status-relays", "Fenster offen?");
 
 function moveRollers(command) {
   var fnPr = particle.callFunction({ deviceId: deviceId, name: 'callShellysOpenClose', argument: command, auth: token });
@@ -41,7 +41,7 @@ function moveRollers(command) {
       console.log('Function called successfully:', data);
       fetchValues();
     }, function (err) {
-      showNotification("Error calling function 'callShellysOpenClose': "+err);
+      showNotification("Error calling function 'callShellysOpenClose': " + err);
     });
 }
 
@@ -54,10 +54,10 @@ function getVar(varName) {
       }
 
     } catch (error) {
-      showNotification('An error occurred while parsing response data: '+error);
+      showNotification('An error occurred while parsing response data: ' + error);
     }
   }, function (err) {
-    showNotification('Device variables cloud not be loaded: '+err);
+    showNotification('Device variables cloud not be loaded: ' + err);
   });
 }
 
@@ -82,7 +82,7 @@ function formatLastConnectionTime(connected, last_heard) {
   let connected_label = connected ? "Online" : "Offline";
   let last_heard_formatted = moment(last_heard).format("LLL");
   $("#connected").html(`<span style='color:${connected_style}'>${connected_label}</span>: Zuletzt gesehen um ${last_heard_formatted}`);
-  
+
 }
 
 function showNotification(text) {
@@ -96,21 +96,20 @@ function hideNotification() {
 }
 
 function getRoomName(roomId) {
-  var roomName ="";
-  $( "#roomSelect option" ).each(function() {
-    if ($(this).val() === roomId){
+  var roomName = "";
+  $("#roomSelect option").each(function () {
+    if ($(this).val() === roomId) {
       roomName = $(this).text();
     }
   });
-  console.log("test: "+roomName);
   return roomName;
 }
 
 function fetchValues() {
-  if (!deviceId){
+  if (!deviceId) {
     showNotification('Choose room to fetch current measurements and enable actions.');
     return;
-  } else{
+  } else {
     hideNotification();
   }
   getVar("roomtemperature");
@@ -122,6 +121,14 @@ function fetchValues() {
   getVar("outsidehumidity");
   getVar("precipitation");
   getVar("gustPeak");
+}
+
+function setLables() {
+  //update labels in table table
+  $("*[data-text]").each(function (index) {
+    let attrName = $(this).attr("data-text");
+    $(this).text(labelMap.get(attrName));
+  });
 }
 
 function doLogin() {
@@ -153,39 +160,41 @@ $(document).ready(function () {
   });
 
   //only used in express.js multiroom context
-  if (Cookies.get('room') && !selectedRoom ) {
+  if (Cookies.get('room') && !selectedRoom) {
     selectedRoom = Cookies.get('room');
     deviceId = deviceMap.get(selectedRoom);
-    labelMap.set('document-title', getRoomName(selectedRoom)+": Fenster open/close");
+    labelMap.set('document-title', getRoomName(selectedRoom) + ": Fenster open/close");
+    setLables();
   }
-  
+
   //only used in express.js multiroom context
   if (!Cookies.get('room') && !selectedRoom) {
     $("#roomSelector").show();
   }
   //only used in express.js multiroom context
   $("#roomSelect").change(function (event) {
-    $( "#roomSelect option:selected" ).each(function() {
-      selectedRoom = $( this ).val();
+    $("#roomSelect option:selected").each(function () {
+      selectedRoom = $(this).val();
     });
     Cookies.set('room', selectedRoom, { expires: 365, path: '' });
     deviceId = deviceMap.get(selectedRoom);
-    labelMap.set('document-title', getRoomName(selectedRoom)+": Fenster open/close");
+    labelMap.set('document-title', getRoomName(selectedRoom) + ": Fenster open/close");
     $("#roomSelector").hide();
     hideNotification();
+    setLables();
     fetchValues();
   });
 
   if (!Cookies.get('password') && !password && !token) {
     $("#login").show();
   }
-  
+
   //only used in static index.html version
-  if (Cookies.get('password') && !token ) {
+  if (Cookies.get('password') && !token) {
     password = Cookies.get('password');
     doLogin();
   }
-  
+
   //only used in static index.html version
   $("#doLogin").click(function () {
     password = $("#password").val().trim();
@@ -194,11 +203,9 @@ $(document).ready(function () {
     doLogin();
   });
 
-  //update data table
-  $("*[data-text]").each(function (index) {
-    let attrName = $(this).attr("data-text");
-    $(this).text(labelMap.get(attrName));
-  });
+
+  // set texts in table/title
+  setLables();
 
   //refresh values every 5 minutes
   setInterval(fetchValues, (5 * 60 * 1000));
